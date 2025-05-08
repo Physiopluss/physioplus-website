df5c10a980d3fa500383b6e3f37300a5489cc5bb
Niten Kumawat <nitenkumawat@gmail.com>
2025-05-08 12:22:30 +0530

    fix : Insurance Partner logos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -155,129 +155,129 @@ const Home = () => {
 				</section>
 
 				<section className="pt-12 md:pt-[108px] pb-11 md:pb-24 px-4 sm:px-12 lg:px-[120px] bg-gradient-to-b from-[#039342] to-[#004E89]">
-  {/* Heading (Visible on all screens) */}
-  <div className="flex flex-col md:flex-row gap-2 md:gap-4 text-white items-center">
-    <div className="flex-1 flex flex-col gap-1">
-      <h6 className="text-3xl md:text-5xl font-bold text-left md:text-left">
-        Pain Relief is Now Just a Click Away
-      </h6>
-      <p className="text-base text-left my-0 lg:pl-0 lg:pr-2">
-        Choose clinic or home care—both options offer expert, personalized physiotherapy for your convenience and comfort
-      </p>
-    </div>
-    <div className="hidden md:block">
-      <Button
-        className="rounded-3xl bg-white text-green flex items-center gap-1"
-        onClick={() =>
-          window.open(
-            "https://play.google.com/store/apps/details?id=com.physioplus.physioplus&pcampaignid=web_share",
-            "_blank"
-          )
-        }
-      >
-        Download our App <FaApple className="w-5 h-5" /> <FaGooglePlay className="w-5 h-5" />
-      </Button>
-    </div>
-  </div>
+					{/* Heading (Visible on all screens) */}
+					<div className="flex flex-col md:flex-row gap-2 md:gap-4 text-white items-center">
+						<div className="flex-1 flex flex-col gap-1">
+							<h6 className="text-3xl md:text-5xl font-bold text-left md:text-left">
+								Pain Relief is Now Just a Click Away
+							</h6>
+							<p className="text-base text-left my-0 lg:pl-0 lg:pr-2">
+								Choose clinic or home care—both options offer expert, personalized physiotherapy for your convenience and comfort
+							</p>
+						</div>
+						<div className="hidden md:block">
+							<Button
+								className="rounded-3xl bg-white text-green flex items-center gap-1"
+								onClick={() =>
+									window.open(
+										"https://play.google.com/store/apps/details?id=com.physioplus.physioplus&pcampaignid=web_share",
+										"_blank"
+									)
+								}
+							>
+								Download our App <FaApple className="w-5 h-5" /> <FaGooglePlay className="w-5 h-5" />
+							</Button>
+						</div>
+					</div>
 
-  {/* Mobile View Box (Visible only on small screens) */}
-<div className="md:hidden max-w-sm mx-auto mt-8">
-<div className="bg-emerald-600 rounded-2xl p-6 text-center text-white shadow-md border border-emerald-600">
-    <h2 className="text-2xl font-bold mb-2">Download Our App Today!</h2>
-    <p className="text-sm mb-6">
-      Unlock exclusive features, seamless booking, and expert physiotherapy care at your fingertips. Stay on track,
-      book sessions effortlessly, and take control of your recovery all in one app.
-    </p>
-    {/* Centered Button */}
-    <div className="flex justify-center">
-      <Button
-        className="rounded-3xl bg-white text-green flex items-center gap-1 normal-case"
-        onClick={() =>
-          window.open(
-            "https://play.google.com/store/apps/details?id=com.physioplus.physioplus&pcampaignid=web_share",
-            "_blank"
-          )
-        }
-      >
-        Download our Patient App <FaGooglePlay className="w-5 h-5" />
-      </Button>
-    </div>
-  </div>
-</div>
+					{/* Mobile View Box (Visible only on small screens) */}
+					<div className="md:hidden max-w-sm mx-auto mt-8">
+						<div className="bg-emerald-600 rounded-2xl p-6 text-center text-white shadow-md border border-emerald-600">
+							<h2 className="text-2xl font-bold mb-2">Download Our App Today!</h2>
+							<p className="text-sm mb-6">
+								Unlock exclusive features, seamless booking, and expert physiotherapy care at your fingertips. Stay on track,
+								book sessions effortlessly, and take control of your recovery all in one app.
+							</p>
+							{/* Centered Button */}
+							<div className="flex justify-center">
+								<Button
+									className="rounded-3xl bg-white text-green flex items-center gap-1 normal-case"
+									onClick={() =>
+										window.open(
+											"https://play.google.com/store/apps/details?id=com.physioplus.physioplus&pcampaignid=web_share",
+											"_blank"
+										)
+									}
+								>
+									Download our Patient App <FaGooglePlay className="w-5 h-5" />
+								</Button>
+							</div>
+						</div>
+					</div>
 
-  {/* Swiper Section (Visible only on medium and larger screens) */}
-  <section className="hidden md:block py-12">
-    <Swiper
-      modules={[Navigation, Pagination]}
-      spaceBetween={10}
-      slidesPerView={1}
-      loop={true}
-      pagination={{ clickable: true }}
-      breakpoints={{
-        540: { slidesPerView: 1, spaceBetween: 10 },
-        720: { slidesPerView: 1, spaceBetween: 10 },
-        960: { slidesPerView: 1, spaceBetween: 10 },
-      }}
-      className="p-0 m-0 overflow-hidden h-full md:h-[520px] max-w-[76vw] sm:max-w-[78vw] md:max-w-[60vw] lg:max-w-[70vw]"
-    >
-      {mobileData.map((data, index) => (
-        <SwiperSlide key={index}>
-          <div className="h-full md:h-[480px] flex flex-col md:flex-row items-center md:items-stretch justify-between px-6 md:px-8 py-6 md:py-8 rounded-lg border border-[#EAEBEC] bg-white bg-opacity-10 backdrop-filter backdrop-blur-md">
-            {/* Text Content */}
-            <div className="w-full md:w-2/3 flex flex-col justify-around text-center md:text-left">
-              <h6
-                className="text-2xl md:text-3xl lg:text-5xl font-bold text-white leading-tight"
-                dangerouslySetInnerHTML={{ __html: data.title }}
-              />
-              <p className="text-xs sm:text-sm md:text-base text-white font-medium my-2 leading-relaxed">
-                {data.description}
-              </p>
+					{/* Swiper Section (Visible only on medium and larger screens) */}
+					<section className="hidden md:block py-12">
+						<Swiper
+							modules={[Navigation, Pagination]}
+							spaceBetween={10}
+							slidesPerView={1}
+							loop={true}
+							pagination={{ clickable: true }}
+							breakpoints={{
+								540: { slidesPerView: 1, spaceBetween: 10 },
+								720: { slidesPerView: 1, spaceBetween: 10 },
+								960: { slidesPerView: 1, spaceBetween: 10 },
+							}}
+							className="p-0 m-0 overflow-hidden h-full md:h-[520px] max-w-[76vw] sm:max-w-[78vw] md:max-w-[60vw] lg:max-w-[70vw]"
+						>
+							{mobileData.map((data, index) => (
+								<SwiperSlide key={index}>
+									<div className="h-full md:h-[480px] flex flex-col md:flex-row items-center md:items-stretch justify-between px-6 md:px-8 py-6 md:py-8 rounded-lg border border-[#EAEBEC] bg-white bg-opacity-10 backdrop-filter backdrop-blur-md">
+										{/* Text Content */}
+										<div className="w-full md:w-2/3 flex flex-col justify-around text-center md:text-left">
+											<h6
+												className="text-2xl md:text-3xl lg:text-5xl font-bold text-white leading-tight"
+												dangerouslySetInnerHTML={{ __html: data.title }}
+											/>
+											<p className="text-xs sm:text-sm md:text-base text-white font-medium my-2 leading-relaxed">
+												{data.description}
+											</p>
 
-              {/* Feature Grid */}
-              <div className="grid grid-cols-1 sm:grid-cols-3 gap-2 my-2 me-2">
-                {data.card.map((data, index) => (
-                  <div
-                    key={index}
-                    className="border border-[#EAEBEC] px-3 py-2 text-xs sm:text-sm text-white rounded-lg flex flex-col items-center justify-around bg-white bg-opacity-20 min-h-20"
-                  >
-                    <img
-                      src={data.cardImg}
-                      alt={data.cardText}
-                      className="w-5 h-5"
-                    />
-                    <div className="text-xs text-center">{data.cardText}</div>
-                  </div>
-                ))}
-              </div>
+											{/* Feature Grid */}
+											<div className="grid grid-cols-1 sm:grid-cols-3 gap-2 my-2 me-2">
+												{data.card.map((data, index) => (
+													<div
+														key={index}
+														className="border border-[#EAEBEC] px-3 py-2 text-xs sm:text-sm text-white rounded-lg flex flex-col items-center justify-around bg-white bg-opacity-20 min-h-20"
+													>
+														<img
+															src={data.cardImg}
+															alt={data.cardText}
+															className="w-5 h-5"
+														/>
+														<div className="text-xs text-center">{data.cardText}</div>
+													</div>
+												))}
+											</div>
 
-              {/* Button */}
-              <Button
-                className="mt-3 w-fit inline-flex items-center justify-center gap-2 bg-white text-green font-normal text-sm md:text-base px-4 py-2.5 rounded-full normal-case"
-                onClick={() =>
-                  window.open(
-                    "https://play.google.com/store/apps/details?id=com.physioplus.physioplus&pcampaignid=web_share",
-                    "_blank"
-                  )
-                }
-              >
-                Download our App <FaApple className="w-5 h-5" /> <FaGooglePlay className="w-4 h-4" />
-              </Button>
-            </div>
+											{/* Button */}
+											<Button
+												className="mt-3 w-fit inline-flex items-center justify-center gap-2 bg-white text-green font-normal text-sm md:text-base px-4 py-2.5 rounded-full normal-case"
+												onClick={() =>
+													window.open(
+														"https://play.google.com/store/apps/details?id=com.physioplus.physioplus&pcampaignid=web_share",
+														"_blank"
+													)
+												}
+											>
+												Download our App <FaApple className="w-5 h-5" /> <FaGooglePlay className="w-4 h-4" />
+											</Button>
+										</div>
 
-            {/* Image */}
-            <div className="w-full md:w-1/3 flex justify-center mt-4 md:mt-0">
-              <img
-                src={data.imageLink}
-                alt="Mobile App"
-                className="max-w-[120px] sm:max-w-[160px] md:max-w-[220px] lg:max-w-[280px] object-contain aspect-[9/16]"
-              />
-            </div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  </section>
-</section>
+										{/* Image */}
+										<div className="w-full md:w-1/3 flex justify-center mt-4 md:mt-0">
+											<img
+												src={data.imageLink}
+												alt="Mobile App"
+												className="max-w-[120px] sm:max-w-[160px] md:max-w-[220px] lg:max-w-[280px] object-contain aspect-[9/16]"
+											/>
+										</div>
+									</div>
+								</SwiperSlide>
+							))}
+						</Swiper>
+					</section>
+				</section>
 
 				{/* free marquee */}
 				<div className="max-w-[100vw] flex justify-center bg-[#025A28]">
@@ -324,11 +324,16 @@ const Home = () => {
 					/>
 					<div className="flex flex-col md:flex-row-reverse justify-between items-center gap-8 xl:gap-14">
 						<div className="mt-10 md:mt-0 mx-auto flex items-center justify-center md:w-1/2 h-full">
-							<img
-								src={"home/insurance.png"}
-								alt={"Get Treated Without Financial Worry"}
-								className="object-contain rounded-md"
-							/>
+							<div className="grid grid-cols-2 sm:grid-cols-3 gap-4 place-items-center">
+								{["TataAIG.png", "HDFCEFRO.png", "Bajaj_Allianz.png", , "ditto.png", "IciciLambard.png"].map((img, idx) => (
+									<img
+										key={idx}
+										src={`home/${img}`}
+										alt={"Get Treated Without Financial Worry"}
+										className="h-36 w-auto object-contain rounded-md"
+									/>
+								))}
+							</div>
 						</div>
 						<TextContainer
 							title={"Get Treated Without Financial Worry"}
@@ -389,7 +394,7 @@ const Home = () => {
 										className="flex items-center"
 									>
 										<span className="font-Oregano text-4xl sm:text-5xl md:text-6xl lg:text-[80px] mx-2 sm:mx-4">
-											Free 
+											Free
 										</span>
 									</div>
 								))}
@@ -402,8 +407,8 @@ const Home = () => {
 									<div
 										key={index}
 										className="flex items-center">
-										<span className ="font-Oregano text-4xl sm:text-5xl md:text-6xl lg:text-[80px] mx-2 sm:mx-4">
-								         Free
+										<span className="font-Oregano text-4xl sm:text-5xl md:text-6xl lg:text-[80px] mx-2 sm:mx-4">
+											Free
 										</span>
 									</div>
 								))}
@@ -433,7 +438,7 @@ const Home = () => {
 							1024: { slidesPerView: 3.2, spaceBetween: 20 },
 						}}
 						className="max-w-[88vw] sm:max-w-[80vw] md:max-w-[80vw] lg:max-w-[72vw]"
-						// navigation
+					// navigation
 					>
 						{painCardData.map((item, index) => (
 							<SwiperSlide key={index}>
