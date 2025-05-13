65736b317ccc8a9bb70690def8dfdf03ff5d60f4
Niten Kumawat <nitenkumawat@gmail.com>
2025-05-13 15:00:54 +0530

    updete:  review card design

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -7,26 +7,21 @@ const ReviewCard = ({ id, name, img, rating, review, patientType }) => {
 			key={id}
 			color="transparent"
 			shadow={false}
-			className="bg-white p-2 text-black text-wrap overflow-hidden"
+			className="bg-white px-6 py-2 text-black text-wrap overflow-hidden rounded-lg shadow-md border border-gray-200 hover:shadow-lg transition-shadow duration-300 ease-in-out min-h-[220px] max-h-[220px]"
 		>
 			<CardHeader
 				color="transparent"
 				floated={false}
 				shadow={false}
-				className="mx-0 flex items-center gap-4 mt-2 pt-0 pb-2"
+				className="mx-0 flex items-center gap-4  pt-2 pb-2"
 			>
-				<Avatar
-					size="sm"
-					variant="circular"
-					src={img}
-					alt={name}
-				/>
+
 				<div className="flex w-full flex-col gap-0.5">
 					<div className="flex flex-col items-start justify-between gap-0.5">
 						<Typography
 							variant="h5"
 							color="blue-gray"
-							className="text-sm font-semibold"
+							className="text-lg font-semibold"
 						>
 							{name}
 						</Typography>
@@ -38,16 +33,29 @@ const ReviewCard = ({ id, name, img, rating, review, patientType }) => {
 						</Typography>
 					</div>
 				</div>
+				<div>
+
+				</div>
+				<Avatar
+					size="md"
+					variant="circular"
+					src={img}
+					alt={name}
+					className="ring-4 ring-gray-300 shadow-md p-0.5 mt-2 mr-2"
+				/>
 			</CardHeader>
 			<CardBody className="flex flex-col gap-4 my-2 p-0 ">
-				<div className="flex items-center gap-4">
+
+
+				<Typography className=" text-sm font-normal   text-pretty h-auto pb-2">{review}</Typography>
+				<div className="flex items-center justify-end gap-4">
 					<div className="flex items-center gap-0.5">
 						{Array(rating)
 							.fill(rating)
 							.map((_, i) => (
 								<FaStar
 									key={i}
-									className="h-3 w-3 text-black"
+									className="h-3 w-3 text-green"
 								/>
 							))}
 						{Array(5 - rating)
@@ -61,8 +69,6 @@ const ReviewCard = ({ id, name, img, rating, review, patientType }) => {
 					</div>
 					<Typography className="text-xs">2 weeks ago</Typography>
 				</div>
-
-				<Typography className=" !text-xs font-normal text-wrap ">{review}</Typography>
 			</CardBody>
 		</Card>
 	);
