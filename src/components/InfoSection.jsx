42fff8b31dec5fef93aea8bcabc65739642a17e2
Niten Kumawat <nitenkumawat@gmail.com>
2025-05-13 15:05:21 +0530

    add: InfoSection component

---
+++ src/components/InfoSection.jsx
@@ -0,0 +1,24 @@
+// InfoSection.js
+
+
+const InfoSection = ({ title, description, iconSrc, iconAlt }) => {
+    return (
+        <div className="my-6">
+            <div className="flex items-center justify-between  my-3">
+                <h3 className="text-xl font-semibold text-[#015C3E]">{title}</h3>
+                <img src={iconSrc} alt={iconAlt} className="w-6 h-6" />
+            </div>
+            <div className="relative  flex items-center w-full h-[2px] my-3">
+                <div className="absolute left-0 top-0 h-[1px] scale-y-[0.5] bg-[#013C2E]" style={{ width: 'fit-content' }}>
+                    <div className="text-xl font-semibold text-transparent select-none whitespace-nowrap">
+                        {title}
+                    </div>
+                </div>
+                <div className="bg-gray-300 w-full h-full"></div>
+            </div>
+            <p className="text-sm text-gray-700 text-pretty mb-9">{description}</p>
+        </div>
+    );
+};
+
+export default InfoSection;
